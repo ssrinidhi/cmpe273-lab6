f7b111e9aa6be64512773486c159d4846568ab2b
ssrinidhi <snehasrinidhi@gmail.com>
2014-11-12 21:51:20 -0800

    Lab3 changes

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,19 +1,36 @@
 package edu.sjsu.cmpe.cache.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.hash.Hashing;
+
 public class Client {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
+    	System.out.println("Starting Cache Client...");
+    	List<DistributedCacheService> listOfServers = new ArrayList<DistributedCacheService>();
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
+        String [] val = {"0","BMW - S class", "BMW - E class", "Audi - 2005", "Toyoto-Prius - 2008", "Toyoto-Corola - 2011", "Porche - 2012", 
+        		            "Ferrari-Red - 2014", "Ford-Taurus - 2014", "Honda-Accord - 2011"};
+        
+      
+        listOfServers.add(new DistributedCacheService("http://localhost:3000"));
+        listOfServers.add(new DistributedCacheService("http://localhost:3001"));
+        listOfServers.add(new DistributedCacheService("http://localhost:3002"));
+        
+        for(int key=1; key<10; key++) 
+        {
+            int bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(key)), listOfServers.size());
+            listOfServers.get(bucket).put(key, val[key]);
+        }
 
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
+        for(int key=1; key<10; key++) 
+        {
+            int bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(key)), listOfServers.size());
+        }
 
-        System.out.println("Existing Cache Client...");
+        System.out.println(" !!<---  HASHING Done. exit--->!!"); 
     }
 
 }
